488814fcd8ece12ba8b865305618fa9ffe98c4b0
Allan Prieto <allanbanilla@gmail.com>
2023-09-25 18:16:23 -0600

    Begginer guie to react

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ Fullstack Curso completo
 
 ## Recursos valiosos
 
+### Cursos:
+- [egghead.io - Cursos](https://egghead.io/q?access_state=free)
+
 ### Para diseño:
 - [UX And HTML5: Help Users Fill in Your Mobile Form](https://www.smashingmagazine.com/2018/08/ux-html5-mobile-form-part-1/)
 - [And extensive Guide to Web Form Usability](https://www.smashingmagazine.com/2011/11/extensive-guide-web-form-usability/)
---
+++ part-1/pruebas.js
@@ -0,0 +1,25 @@
+
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = newValue => {
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+    </div>
+  )
+}
