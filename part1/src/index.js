0c84d8f90e3feb1cafb6bcb904d5d584f8ba66a7
Allan Prieto <allanbanilla@gmail.com>
2023-10-03 14:04:31 -0600

    Funciones y estados

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,22 +1,29 @@
-import React from 'react';
+// import React from 'react';
+// Componentes con estado
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
-  // Funcion auxiliar
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-  }
+// ** Función auxiliar y Desestructuración
+ 
+/*const Hello = ({name, age}) => {
+  // Desestruccturar para optimizar, y nos ayuda a reunir los valores de un objeto en variables
+  // separadas..
+  // const name = props.name
+  // const age = props.age
+  //const {name, age} = props
+
+  // Funcion auxiliar -- Definir una funció dentro de otra en Java Scripts es muy comun
+  const bornYear = () => new Date().getFullYear() - age
 
   return(
     <>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-      <p>So you were probably born in {bornYear}</p>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
     </>
   )
-}
+}*/
 
-const App = () => {
+/*const App = () => {
 
   const name = 'Peter'
   const age = 10
@@ -29,6 +36,58 @@ const App = () => {
     <Hello name={name} age={age}/>
   </>
   )
+}*/
+
+// ** Re-renderizado de la página
+/*
+const AppCounter = (props) => {
+  const {counter} = props
+  return(
+    <div>{counter}</div>
+  )
+}
+
+let counter = 1
+
+// ReactDOM.render(<App />, document.getElementById('root'))
+const refresh = () => {
+  ReactDOM.render(<AppCounter counter={counter} />, document.getElementById('root'))
+}*/
+
+// Hace llamadas a la funcion que renderiza, para mostrar la suma cada cierto tiempo
+// Sin embargo no es la forma recomendada de renderizar este objeto
+/*
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000) */
+
+// ** Componente con estado
+
+const App = () => {
+  // sintaxis de asignación de desestructuración
+  const [ counter, setCounter ] = useState(0)
+
+  // Separamos controladores de eventos en funciones
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+
+  // Usaremos eventos para mostrar y aumentar el cunter
+  return (
+    <>
+    <div>{counter}</div>
+    
+    <button onClick={increaseByOne}>
+      plus
+    </button>
+    <button onClick={setToZero}>
+      Start
+    </button>
+    </>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+// https://fullstackopen.com/es/part1/estado_del_componente_controladores_de_eventos#funciones-auxiliares-del-componente
+
