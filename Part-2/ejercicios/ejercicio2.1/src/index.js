42f0132bf7d5321221a4546c68e3a9a3644dfe83
Allan19Prieto <allanbanilla@gmail.com>
2023-11-08 00:43:01 -0200

    proceso Ejercicios 2.1.-2.5.

--- Part-2/ejercicios/ejercicio2.1/src/index.js
+++ Part-2/ejercicios/ejercicio2.1/src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Header = (props) => {
+  return <h1>{props.name}</h1>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Content = (props) => {
+  return(
+    <>
+      <Part name={props.parts[0].name} excer={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} excer={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} excer={props.parts[2].exercises} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises }</p>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.name} {props.excer}</p>
+    </>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App/>
+)
